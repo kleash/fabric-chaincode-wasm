2342ef31315eb498c142e15d846572fe24474c91
Shubham Aggarwal <shubham6300@gmail.com>
2019-10-17 14:52:36 +0800

    integration tests - Use nwo for bootstrapping fabric network - Use solo network setup - install wasmcc - install sample rust wasm chaincode - invoke and query installed chaincode
    Signed-off-by: Shubham Aggarwal <shubham6300@gmail.com>

---
+++ integration/e2e/e2e_suite_test.go
@@ -0,0 +1,34 @@
+package e2e
+
+import (
+	"encoding/json"
+	"github.com/hyperledger/fabric/integration/nwo"
+	"testing"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+func TestE2e(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "E2e Suite")
+}
+
+var components *nwo.Components
+
+var _ = SynchronizedBeforeSuite(func() []byte {
+	components = Build()
+
+	payload, err := json.Marshal(components)
+	Expect(err).NotTo(HaveOccurred())
+
+	return payload
+}, func(payload []byte) {
+	err := json.Unmarshal(payload, &components)
+	Expect(err).NotTo(HaveOccurred())
+})
+
+var _ = SynchronizedAfterSuite(func() {
+}, func() {
+	components.Cleanup()
+})
